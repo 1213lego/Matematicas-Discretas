55e4f6ca5846189d8e11600ac069af418f705820
1213lego <36804989+1213lego@users.noreply.github.com>
2018-05-20 12:19:29 -0500

    Creandografo(30%)

---
+++ grafos/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttcrearVertices = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttCrearGrafo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttcrearVertices
+            // 
+            this.buttcrearVertices.Location = new System.Drawing.Point(12, 38);
+            this.buttcrearVertices.Name = "buttcrearVertices";
+            this.buttcrearVertices.Size = new System.Drawing.Size(82, 24);
+            this.buttcrearVertices.TabIndex = 0;
+            this.buttcrearVertices.Text = "Crear Vertices";
+            this.buttcrearVertices.UseVisualStyleBackColor = true;
+            this.buttcrearVertices.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(295, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(440, 300);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentdoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // buttCrearGrafo
+            // 
+            this.buttCrearGrafo.Location = new System.Drawing.Point(100, 38);
+            this.buttCrearGrafo.Name = "buttCrearGrafo";
+            this.buttCrearGrafo.Size = new System.Drawing.Size(75, 23);
+            this.buttCrearGrafo.TabIndex = 3;
+            this.buttCrearGrafo.Text = "Crear Grafo";
+            this.buttCrearGrafo.UseVisualStyleBackColor = true;
+            this.buttCrearGrafo.Click += new System.EventHandler(this.buttCrearGrafo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "&N° Vertices";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 524);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttCrearGrafo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttcrearVertices);
+            this.Name = "Form1";
+            this.Text = "Grafos";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttcrearVertices;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttCrearGrafo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
